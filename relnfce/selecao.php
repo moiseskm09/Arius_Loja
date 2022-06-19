aa8a4a8eecca8409367cc857246ff9b57e528e72
moiseskm09 <moiseskm09@gmail.com>
2022-06-18 23:29:54 -0300

    tefcons

--- relnfce/selecao.php
+++
@@ -1,5 +0,0 @@
-<?php
-	/* prepara o documento para comunicação com o JSON, as duas linhas a seguir são obrigatórias 
-	  para que o PHP saiba que irá se comunicar com o JSON, elas sempre devem estar no ínicio da página */
-	
-?>
